092d0d65b9b8307f58e51d37873b9fa6ed9aaa48
Mikael Figueroa <mifiguer@uw.edu>
2021-03-15 11:14:33 -0700

    Added ResNet acoustic model, multi-processing dataloaders, best validation loss weight saving.

--- resnet_am.py
+++ resnet_am.py
@@ -23,24 +23,21 @@ class GroupNormWrapper(nn.Module):
     def bias(self):
         return self.norm.bias
 
-class ResNetLSTM_AcousticModel(nn.Module):
-    def __init__(self, lstm_hidden_dim, spectral_filter_dims, min_output_word_id, max_word_id, num_lstm_layers=None, resnet_backbone_name=None, bottleneck_size=None):
-        super(ResNetLSTM_AcousticModel, self).__init__()
-        if num_lstm_layers is None:
-            num_lstm_layers = 1
+class ResNet_AcousticModel(nn.Module):
+    def __init__(self, spectral_filter_dims, min_output_word_id, max_word_id, resnet_backbone_name=None, bottleneck_size=None):
+        super(ResNet_AcousticModel, self).__init__()
         if resnet_backbone_name is None:
             resnet_backbone_name = 'resnet50'
         if bottleneck_size is None:
-            bottleneck_size = 4
+            bottleneck_size = 32
         
         resnet_backbone_name_to_constructor = {
             'resnet18' : resnet_1d.resnet18,
-            'resnet50' : resnet_1d.resnet50
+            'resnet50' : resnet_1d.resnet50,
+            'resnext101' : resnet_1d.resnext101_32x8d
         }
         resnet_backbone = resnet_backbone_name_to_constructor[resnet_backbone_name]
         
-        self.num_lstm_layers = num_lstm_layers
-        self.lstm_hidden_dim = lstm_hidden_dim
         self.bottleneck_size = bottleneck_size
         self.max_word_id = max_word_id
         self.min_output_word_id = min_output_word_id
@@ -48,41 +45,35 @@ class ResNetLSTM_AcousticModel(nn.Module):
         
         input_channels = self.spectral_filter_dims
         self.backbone = resnet_backbone(include_top=False, norm_layer=GroupNormWrapper, input_channels=input_channels, zero_init_residual=True, replace_stride_with_dilation=[True,True,True])
+        self.backbone_output_channels = self.backbone.get_output_channels()
         
-        # The LSTM takes CNN embeddings as inputs, and outputs hidden states
-        # with dimensionality hidden_dim.
-        self.lstm = nn.LSTM(self.backbone.get_output_channels(), self.lstm_hidden_dim, num_layers=self.num_lstm_layers)
         if self.bottleneck_size > 0:
-            self.lstm2bottleneck = nn.Linear(self.lstm_hidden_dim, self.bottleneck_size)
+            self.backbone2bottleneck = nn.Linear(self.backbone_output_channels, self.bottleneck_size)
             bottleneck2word_input_size = self.bottleneck_size
         else:
-            self.lstm2bottleneck = None
-            bottleneck2word_input_size = self.lstm_hidden_dim
+            self.backbone2bottleneck = None
+            bottleneck2word_input_size = self.backbone.get_output_channels()
         
         # The linear layer that maps from hidden state space to word space
         self.bottleneck2word = nn.Linear(bottleneck2word_input_size, self.max_word_id - self.min_output_word_id + 1) # outputs range from [self.min_output_word_id, self.max_word_id]
     
     def forward(self, spectral_input):
-        resnet_embeddings = self.backbone(spectral_input)
-        print(f'spectral_input: {spectral_input.size()} -> resnet_embeddings: {resnet_embeddings.size()}')
+        resnet_output = self.backbone(spectral_input)
+        #print(f'spectral_input: {spectral_input.size()} -> resnet_output: {resnet_output.size()}')
         
-        resnet_output_seq_length = resnet_embeddings.size()[-1]
-        spectral_input_seq_length = spectral_input.size()[-1]
-        #print(f'ResNet input length: {spectral_input_seq_length} -> {resnet_output_seq_length}')
+        bottleneck_input = torch.transpose(resnet_output, 0, 2)
+        bottleneck_input = torch.transpose(bottleneck_input, 1, 2)
         
-        lstm_input = torch.transpose(resnet_embeddings, 0, 2)
-        lstm_input = torch.transpose(lstm_input, 1, 2)
-        lstm_out, _ = self.lstm(lstm_input)
-        batch_size = lstm_out.size()[0]
-        sequence_length = lstm_out.size()[1]
+        sequence_length = bottleneck_input.size()[0]
+        batch_size = bottleneck_input.size()[1]
         
-        lstm_out = lstm_out.view(-1, self.lstm_hidden_dim)
+        bottleneck_input = bottleneck_input.view(-1, self.backbone_output_channels)
         
         if self.bottleneck_size > 0:
-            bottleneck = self.lstm2bottleneck(lstm_out)
+            bottleneck = self.backbone2bottleneck(bottleneck_input)
         else:
-            bottleneck = lstm_out
+            bottleneck = bottleneck_input
         
         word_space = self.bottleneck2word(bottleneck)
-        word_scores = F.log_softmax(word_space.view(batch_size, sequence_length, -1), dim=2)
+        word_scores = F.log_softmax(word_space.view(sequence_length, batch_size, -1), dim=2)
         return word_scores
--- train_simple_conv_lstm_am.py
+++ train_simple_conv_lstm_am.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import pytorch_warmup as lr_warmup
 
 from torch.utils.data import DataLoader, RandomSampler
 from torchvision import transforms, utils
@@ -17,155 +18,119 @@ from torch_transforms import DeleteSequencePrefix, ToTensor, RemapUsingMinWordID
 
 from conv_lstm_am import ConvLSTM_AcousticModel
 from resnet_lstm_am import ResNetLSTM_AcousticModel
+from resnet_am import ResNet_AcousticModel
 from vocabulary import VocabularySpecialWords
 from tqdm import tqdm
 
-torch.manual_seed(1234)
-np.random.seed(1234)
-
-parser = argparse.ArgumentParser(description='Train a simple ConvLSTM acoustic model.')
-parser.add_argument('log_file', help='Path to output log file')
-parser.add_argument('train_dataset', help='Path to processed train dataset file')
-parser.add_argument('valid_dataset', help='Path to processed validation dataset file')
-parser.add_argument('lm_train_dataset', help='Path to processed language model train dataset file')
-parser.add_argument('lm_valid_dataset', help='Path to processed language model train dataset file')
-parser.add_argument('--vocab_unk_rate', help='UNKing rate to use for vocabulary, by default will use true UNK rate based on validation set OOV rate', default=-1.0)
-args = parser.parse_args()
-
-n_epochs = 50#1000
-train_samples_per_epoch = 1000
-valid_samples_per_epoch = 100
-batch_size = 2
-max_sequence_length = 50
-
-lm_train_dataset = TextDataset(args.lm_train_dataset, max_sequence_length)
-lm_valid_dataset = TextDataset(args.lm_valid_dataset, max_sequence_length)
-
-if args.vocab_unk_rate == -1.0:
-    lm_train_dataset.unk_vocabulary_with_true_oov_rate(lm_valid_dataset)
-elif args.vocab_unk_rate > 0:
-    lm_train_dataset.unk_vocabulary_with_oov_rate(args.vocab_unk_rate)
-
-train_dataset = SpeechDataset(args.train_dataset, vocabulary=lm_train_dataset.vocabulary)
-valid_dataset = SpeechDataset(args.valid_dataset, vocabulary=lm_train_dataset.vocabulary)
-max_dataset_input_length = max(train_dataset.get_max_input_length(), valid_dataset.get_max_input_length())
-max_dataset_target_length = max(train_dataset.get_max_transcription_length(), valid_dataset.get_max_transcription_length())
-
-max_word_id = train_dataset.vocabulary.get_max_word_id()
-
-# need to leave 0 for a BLANK symbol for CTC decoding, and thus STOP should map to 1 not 0
-lm_min_word_id = VocabularySpecialWords.STOP.value - 1 #train_dataset.vocabulary.get_min_valid_lm_output_word_id()
-
-vocabulary_size = train_dataset.vocabulary.get_vocab_size()
-valid_dataset.use_vocabulary_from_dataset(train_dataset)
-print(f'Vocabulary Size: {vocabulary_size}')
-
-dataset_transformer = transforms.Compose([
-    DeleteSequencePrefix('target', 1), # target should not include START symbol at beginning
-    RemapUsingMinWordID('target',lm_min_word_id),
-    ToTensor()])
-
-train_dataset.set_transform(dataset_transformer)
-valid_dataset.set_transform(dataset_transformer)
-
-train_sampler = RandomSampler(train_dataset, replacement=True, num_samples=train_samples_per_epoch)
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler, num_workers=0, collate_fn=PadSequencesCollater())
-valid_sampler = RandomSampler(valid_dataset, replacement=True, num_samples=valid_samples_per_epoch)
-valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, sampler=valid_sampler, num_workers=0, collate_fn=PadSequencesCollater())
-
-spectogram_num_filters = 40
-
-num_lstm_layers = np.arange(1,5)
-num_conv_blocks = np.arange(0,1) #np.arange(2,4)
-num_conv_layers_per_block = np.arange(1,4)
-hidden_sizes = np.arange(16, 64)
-learning_rates = 10.**np.arange(-5,-2)
-hyperparameters_tried = set()
-
-logfile_prefix = os.path.splitext(args.log_file)[0]
-logfile_dir = os.path.dirname(args.log_file)
-
-while True:
-    random_hidden_size = np.random.choice(hidden_sizes)
-    random_learning_rate = np.random.choice(learning_rates)
-    random_num_lstm_layers = np.random.choice(num_lstm_layers)
-    #random_num_conv_blocks = np.random.choice(num_conv_blocks)
-    #random_num_conv_layers_per_block = np.random.choice(num_conv_layers_per_block)
-    hyperparameter_combo = (random_hidden_size, random_learning_rate, random_num_lstm_layers)#, random_num_conv_blocks, random_num_conv_layers_per_block)
-    if hyperparameter_combo in hyperparameters_tried:
-        continue
-    hyperparameters_tried.add(hyperparameter_combo)
+if __name__ == '__main__':
+    torch.manual_seed(1234)
+    np.random.seed(1234)
     
-    #total_conv_layers = random_num_conv_blocks*random_num_conv_layers_per_block
-    model_suffix = f'hidden_{random_hidden_size}_lr_{random_learning_rate}_lstm_layers_{random_num_lstm_layers}_resnet18'
-    model_save_name = f'model_{model_suffix}.pth'
-    model_save_path = os.path.join(logfile_dir, model_save_name)
-    if os.path.isfile(model_save_path):
-        continue
+    parser = argparse.ArgumentParser(description='Train a simple ConvLSTM acoustic model.')
+    parser.add_argument('log_file', help='Path to output log file')
+    parser.add_argument('train_dataset', help='Path to processed train dataset file')
+    parser.add_argument('valid_dataset', help='Path to processed validation dataset file')
+    parser.add_argument('lm_train_dataset', help='Path to processed language model train dataset file')
+    parser.add_argument('lm_valid_dataset', help='Path to processed language model train dataset file')
+    parser.add_argument('--vocab_unk_rate', help='UNKing rate to use for vocabulary, by default will use true UNK rate based on validation set OOV rate', default=-1.0)
+    args = parser.parse_args()
     
-    model = ResNetLSTM_AcousticModel(random_hidden_size, spectogram_num_filters, lm_min_word_id, max_word_id,
-        num_lstm_layers=random_num_lstm_layers, bottleneck_size=0)
-    model.cuda()
-    model_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f'\n\nTotal Parameters: {model_total_params}\n\n' + str(model))
+    n_epochs = 1000
+    train_samples_per_epoch = 40000
+    valid_samples_per_epoch = 100
+    batch_size = 1
+    max_sequence_length = 50
     
-    loss_function = nn.CTCLoss(zero_infinity=True)
-    optimizer = optim.Adam(model.parameters(), lr=random_learning_rate)
+    lm_train_dataset = TextDataset(args.lm_train_dataset, max_sequence_length)
+    lm_valid_dataset = TextDataset(args.lm_valid_dataset, max_sequence_length)
     
-    logfile_path = f'{logfile_prefix}_{model_suffix}.txt'
-    log_file = open(logfile_path, 'w')
-    model_samples_filepath = f'{logfile_prefix}_output_samples_{model_suffix}.txt'
-    model_samples_file = open(model_samples_filepath, 'w')
+    if args.vocab_unk_rate == -1.0:
+        lm_train_dataset.unk_vocabulary_with_true_oov_rate(lm_valid_dataset)
+    elif args.vocab_unk_rate > 0:
+        lm_train_dataset.unk_vocabulary_with_oov_rate(args.vocab_unk_rate)
     
-    log_file.write(f'Model Total Parameters: {model_total_params}\n')
-    log_file.write(f'Epoch #,Train Average Loss,Validation Average Loss\n')
-    log_file.flush()
-    model.train()
-    for epoch in range(n_epochs):
-        train_number_of_words = 0
-        batches_loop = tqdm(train_dataloader)
-        total_train_loss = 0
-        num_train_batches = 0
+    train_dataset = SpeechDataset(args.train_dataset, vocabulary=lm_train_dataset.vocabulary)
+    valid_dataset = SpeechDataset(args.valid_dataset, vocabulary=lm_train_dataset.vocabulary)
+    max_dataset_input_length = max(train_dataset.get_max_input_length(), valid_dataset.get_max_input_length())
+    max_dataset_target_length = max(train_dataset.get_max_transcription_length(), valid_dataset.get_max_transcription_length())
+    
+    max_word_id = train_dataset.vocabulary.get_max_word_id()
+    
+    # need to leave 0 for a BLANK symbol for CTC decoding, and thus STOP should map to 1 not 0
+    lm_min_word_id = VocabularySpecialWords.STOP.value - 1 #train_dataset.vocabulary.get_min_valid_lm_output_word_id()
+    
+    vocabulary_size = train_dataset.vocabulary.get_vocab_size()
+    valid_dataset.use_vocabulary_from_dataset(train_dataset)
+    print(f'Vocabulary Size: {vocabulary_size}')
+    
+    dataset_transformer = transforms.Compose([
+        DeleteSequencePrefix('target', 1), # target should not include START symbol at beginning
+        RemapUsingMinWordID('target',lm_min_word_id),
+        ToTensor()])
+    
+    train_dataset.set_transform(dataset_transformer)
+    valid_dataset.set_transform(dataset_transformer)
+    
+    train_sampler = RandomSampler(train_dataset, replacement=True, num_samples=train_samples_per_epoch)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler, num_workers=12, collate_fn=PadSequencesCollater())
+    valid_sampler = RandomSampler(valid_dataset, replacement=True, num_samples=valid_samples_per_epoch)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, sampler=valid_sampler, num_workers=0, collate_fn=PadSequencesCollater())
+    
+    spectogram_num_filters = 40
+    
+    bottleneck_size = 2**np.arange(3,10)
+    learning_rates = 10.**np.arange(-5,-2)
+    hyperparameters_tried = set()
+    
+    logfile_prefix = os.path.splitext(args.log_file)[0]
+    logfile_dir = os.path.dirname(args.log_file)
+    
+    while True:
+        random_bottleneck_size = np.random.choice(bottleneck_size)
+        random_learning_rate = np.random.choice(learning_rates)
+        hyperparameter_combo = (random_learning_rate,random_bottleneck_size)
+        if hyperparameter_combo in hyperparameters_tried:
+            continue
+        hyperparameters_tried.add(hyperparameter_combo)
         
-        for batch_sample in batches_loop:
-            model.zero_grad()
-            model_inputs = batch_sample['input'].cuda()
-            model_targets = batch_sample['target'].cuda()
-            model_target_lengths = batch_sample['target_lengths'].cuda()
-            
-            word_scores = model(model_inputs)
-            ctc_loss_input_length_dim = word_scores.size()[0]
-            ctc_loss_input_length = torch.LongTensor([ctc_loss_input_length_dim]).repeat(batch_size).cuda()
-            
-            loss = loss_function(word_scores, model_targets, ctc_loss_input_length, model_target_lengths)
-            loss.backward()
-            optimizer.step()
-            
-            with torch.no_grad():
-                if np.random.uniform(0,1) >= 0.97:
-                    targets = model_targets[0,:].cpu().numpy() + lm_min_word_id
-                    target_sentence = train_dataset.vocabulary.tokens_to_sentence(targets)
-                    model_samples_file.write(f'\n\nTarget Sentence:\n\t{target_sentence}\n\n')
-                    model_outputs = torch.argmax(word_scores[:, 0, :], dim=1).cpu().numpy() + lm_min_word_id
-                    model_output_sentence = train_dataset.vocabulary.tokens_to_sentence(model_outputs)
-                    model_output_sentence = model_output_sentence.replace('[START]','[BLANK]')
-                    model_output_sentence = re.sub(r'(\[BLANK\] ?)+', '[BLANK] ', model_output_sentence)
-                    model_output_sentence = model_output_sentence.replace('  ', ' ')
-                    model_samples_file.write(f'\n\nModel Output Sentence:\n\t{model_output_sentence}\n\n')
-                    model_samples_file.flush()
-                total_train_loss += loss
-            
-            batches_loop.set_description('Train Epoch {}/{}'.format(epoch + 1, n_epochs))
-            batches_loop.set_postfix(loss=loss.item())
-            num_train_batches += 1
+        model_suffix = f'bottleneck_{random_bottleneck_size}_lr_{random_learning_rate}_resnext101'
+        model_save_name = f'model_{model_suffix}.pth'
+        model_save_path = os.path.join(logfile_dir, model_save_name)
+        if os.path.isfile(model_save_path):
+            continue
         
-        valid_number_of_words = 0
-        batches_loop = tqdm(valid_dataloader)
-        total_valid_loss = 0
-        with torch.no_grad():
-            avg_train_loss = total_train_loss / num_train_batches
-            num_valid_batches = 0
+        model = ResNet_AcousticModel(spectogram_num_filters, lm_min_word_id, max_word_id, bottleneck_size=random_bottleneck_size, resnet_backbone_name='resnext101')
+        model.cuda()
+        model_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print(f'\n\nTotal Parameters: {model_total_params}\n\n' + str(model))
+        
+        loss_function = nn.CTCLoss(zero_infinity=True)
+        optimizer = optim.Adam(model.parameters(), lr=random_learning_rate)
+        
+        logfile_path = f'{logfile_prefix}_{model_suffix}.txt'
+        log_file = open(logfile_path, 'w')
+        model_samples_filepath = f'{logfile_prefix}_output_samples_{model_suffix}.txt'
+        model_samples_file = open(model_samples_filepath, 'w')
+        
+        log_file.write(f'Model Total Parameters: {model_total_params}\n')
+        log_file.write(f'Epoch #,Train Average Loss,Validation Average Loss\n')
+        log_file.flush()
+        model.train()
+        
+        validation_losses = []
+        best_validation_loss = np.inf
+        
+        num_train_steps = len(train_dataloader) * n_epochs
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_train_steps)
+        warmup_scheduler = lr_warmup.UntunedLinearWarmup(optimizer)
+        for epoch in range(n_epochs):
+            train_number_of_words = 0
+            batches_loop = tqdm(train_dataloader)
+            total_train_loss = 0
+            num_train_batches = 0
+            
             for batch_sample in batches_loop:
+                model.zero_grad()
                 model_inputs = batch_sample['input'].cuda()
                 model_targets = batch_sample['target'].cuda()
                 model_target_lengths = batch_sample['target_lengths'].cuda()
@@ -175,15 +140,71 @@ while True:
                 ctc_loss_input_length = torch.LongTensor([ctc_loss_input_length_dim]).repeat(batch_size).cuda()
                 
                 loss = loss_function(word_scores, model_targets, ctc_loss_input_length, model_target_lengths)
+                loss.backward()
+                optimizer.step()
+                lr_scheduler.step()
+                warmup_scheduler.dampen()
                 
-                total_valid_loss += loss
+                with torch.no_grad():
+                    if np.random.uniform(0,1) >= 0.97:
+                        targets = model_targets[0,:].cpu().numpy() + lm_min_word_id
+                        target_sentence = train_dataset.vocabulary.tokens_to_sentence(targets)
+                        model_samples_file.write(f'\n\nTarget Sentence:\n\t{target_sentence}\n\n')
+                        model_outputs = torch.argmax(word_scores[:, 0, :], dim=1).cpu().numpy() + lm_min_word_id
+                        model_output_sentence = train_dataset.vocabulary.tokens_to_sentence(model_outputs)
+                        model_output_sentence = model_output_sentence.replace('[START]','[BLANK]')
+                        model_output_sentence = re.sub(r'(\[BLANK\] ?)+', '[BLANK] ', model_output_sentence)
+                        model_output_sentence = model_output_sentence.replace('  ', ' ')
+                        model_samples_file.write(f'\n\nModel Output Sentence:\n\t{model_output_sentence}\n\n')
+                        model_samples_file.flush()
+                    total_train_loss += loss
                 
-                batches_loop.set_description('Validation Epoch {}/{}'.format(epoch + 1, n_epochs))
-                batches_loop.set_postfix(loss=f'{loss.item():.2f}')
-                num_valid_batches += 1
+                batches_loop.set_description('Train Epoch {}/{}'.format(epoch + 1, n_epochs))
+                batches_loop.set_postfix(loss=loss.item())
+                num_train_batches += 1
             
-            avg_valid_loss = total_valid_loss / num_valid_batches
-            log_file.write(f'{epoch+1},{avg_train_loss.cpu().numpy()},{total_valid_loss.cpu().numpy()}\n')
-            log_file.flush()
-    
-    torch.save(model.state_dict(), model_save_path)
+            valid_number_of_words = 0
+            batches_loop = tqdm(valid_dataloader)
+            total_valid_loss = 0
+            with torch.no_grad():
+                avg_train_loss = total_train_loss / num_train_batches
+                avg_train_loss = avg_train_loss.cpu().numpy()
+                num_valid_batches = 0
+                for batch_sample in batches_loop:
+                    model_inputs = batch_sample['input'].cuda()
+                    model_targets = batch_sample['target'].cuda()
+                    model_target_lengths = batch_sample['target_lengths'].cuda()
+                    
+                    word_scores = model(model_inputs)
+                    ctc_loss_input_length_dim = word_scores.size()[0]
+                    ctc_loss_input_length = torch.LongTensor([ctc_loss_input_length_dim]).repeat(batch_size).cuda()
+                    
+                    loss = loss_function(word_scores, model_targets, ctc_loss_input_length, model_target_lengths)
+                    
+                    total_valid_loss += loss
+                    
+                    batches_loop.set_description('Validation Epoch {}/{}'.format(epoch + 1, n_epochs))
+                    batches_loop.set_postfix(loss=f'{loss.item():.2f}')
+                    num_valid_batches += 1
+                
+                avg_valid_loss = total_valid_loss / num_valid_batches
+                avg_valid_loss = avg_valid_loss.cpu().numpy()
+                validation_losses.append(avg_valid_loss)
+                
+                if avg_valid_loss < best_validation_loss:
+                    print(f'New Best Avg Validation Loss: {avg_valid_loss:.6}')
+                    best_validation_loss = avg_valid_loss
+                    model_epoch_save_name = f'model_epoch_{epoch+1}_{model_suffix}_val_{avg_valid_loss:.5}.pth'
+                    model_epoch_save_path = os.path.join(logfile_dir, model_epoch_save_name)
+                    torch.save({
+                        'epoch': epoch,
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        'avg_train_loss': avg_train_loss,
+                        'avg_valid_loss': avg_valid_loss
+                        }, model_epoch_save_path)
+                
+                log_file.write(f'{epoch+1},{avg_train_loss},{avg_valid_loss}\n')
+                log_file.flush()
+        
+        torch.save(model.state_dict(), model_save_path)
